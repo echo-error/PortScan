6c4744c8ba7b778dcf0937ef010da58085284867
echo-error <echo.error@gmail.com>
2018-07-04 15:10:16 +0800

    fix a throw exception report openport number scan all ports by default

--- ScanPort.py
+++ ScanPort.py
@@ -4,6 +4,8 @@ import threading
 
 
 screenLock=threading.Semaphore(value=1)
+#global open_port_count
+open_port_count=0
 def connScan(tgtHost,tgtPort):
 
 
@@ -13,12 +15,17 @@ def connScan(tgtHost,tgtPort):
         connSkt.connect((tgtHost,tgtPort))
         connSkt.send('ViolentPython\r\n')
         results=connSkt.recv(100)
+        global open_port_count
+        open_port_count = open_port_count + 1
         screenLock.acquire()
+
         print('[+] %d/tcp open' %tgtPort)
         print('[+] %s'  %str(results))
     except:
         screenLock.acquire()
-        print('[-] %d/tcp closed' %tgtPort)
+
+
+       # print('[-] %d/tcp closed' %tgtPort)
 
     finally:
         screenLock.release()
@@ -35,20 +42,27 @@ def main():
 
     parser=optparse.OptionParser("%prog -H <target host> -p <target port>")
     parser.add_option('-H',dest='tgtHost',type='string',help='specify target host')
-#    parser.add_option('-p' ,dest='tgtPort',type='string',help='specify target port[s] separated by comma')
+#   parser.add_option('-p' ,dest='tgtPort',type='string',help='specify target port[s] separated by comma')
     parser.add_option('-p' ,dest='tgtPorts',type='string',help='specify target port[s] separated by comma')
     (options,args)=parser.parse_args()
     tgtHost = options.tgtHost
-#    tgtPorts=str(options.tgtPort).split(',')
+#   tgtPorts=str(options.tgtPort).split(',')
     print('the ports list is %s' %options.tgtPorts)
     print('the type of options.tgtPorts is ' , type(options.tgtPorts) )
     tgtPorts=str(options.tgtPorts).split(',')
 
-    if (tgtHost == None) | (tgtPorts[0] == None):
+#    if (tgtHost == None) | (options.tgtPorts == None):
+#        print parser.usage
+#        exit(1)
+
+    if (tgtHost == None):
         print parser.usage
         exit(1)
+    if (options.tgtPorts == None):
+        tgtPorts=range(1,65534)
     portScan(tgtHost,tgtPorts)
-
+#    global open_port_count
+    print "The Open Port number is %d" %open_port_count
 
 if __name__ == '__main__':
         main()
